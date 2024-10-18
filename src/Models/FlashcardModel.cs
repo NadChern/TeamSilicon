0887a000c7678be31eb52c59a534b859ca5e15f4
Nadin Chernova <nchernova2702@gmail.com>
2024-10-18 16:09:56 -0700

    Added comments to FlashcardModel in FlashcardModel.cs

--- src/Models/FlashcardModel.cs
+++ src/Models/FlashcardModel.cs
@@ -1,20 +1,32 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// FlashcardModel represents a single flashcard with its properties
+    /// </summary>
     public class FlashcardModel
     {
+        // Primary key Id
         public string Id { get; set; }
+
+        // Id of the category the card belong to
         public string CategoryId { get; set; }
+
+        // Question on the flashcard
         public string Question { get; set; }
+
+        // Answer on the flashcard
         public string Answer { get; set; }
-        
-        [JsonPropertyName("Difficulty")]
-        public string DifficultyLevel { get; set; }
-        
+
+        // Difficulty level of the question
+        [JsonPropertyName("Difficulty")] public string DifficultyLevel { get; set; }
+
+        // Link to additional resources related to the flashcard (optional)
         public string Url { get; set; }
+
+        // Returns the string representation of the flashcard
         public override string ToString() => JsonSerializer.Serialize<FlashcardModel>(this);
     }
 }
