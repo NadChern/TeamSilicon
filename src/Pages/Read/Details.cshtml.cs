4ed1159c772cc815aa39a3747e3fa52bafa013b3
Dat Nguyen <dnguyen8@seattleu.edu>
2024-10-19 19:36:25 -0700

    Added Details.cshtml.cs to retrieve flashcard details in Read folder

---
+++ src/Pages/Read/Details.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Read
+{
+    public class DetailsModel : PageModel
+    {
+        public JsonFileFlashcardService FlashcardService { get; }
+
+        public FlashcardModel Flashcard { get; private set; }
+
+        public DetailsModel(JsonFileFlashcardService flashcardService)
+        {
+            FlashcardService = flashcardService;
+        }
+
+        public void OnGet(string id)
+        {
+            Flashcard = FlashcardService.GetById(id);
+
+        }
+    }
+}
