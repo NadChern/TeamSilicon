b27cb75ce9ae40ebac81e39e661a693c1a5af1c8
Nadin Chernova <nchernova2702@gmail.com>
2024-10-20 17:46:05 -0700

    Removed unused functionality from JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -2,14 +2,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
-
 using ContosoCrafts.WebSite.Models;
-
 using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
 {
-   public class JsonFileProductService
+    public class JsonFileProductService
     {
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
@@ -25,7 +23,7 @@ namespace ContosoCrafts.WebSite.Services
 
         public IEnumerable<ProductModel> GetAllData()
         {
-            using(var jsonFileReader = File.OpenText(JsonFileName))
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
@@ -35,60 +33,6 @@ namespace ContosoCrafts.WebSite.Services
             }
         }
 
-        /// <summary>
-        /// Add Rating
-        /// 
-        /// Take in the product ID and the rating
-        /// If the rating does not exist, add it
-        /// Save the update
-        /// </summary>
-        /// <param name="productId"></param>
-        /// <param name="rating"></param>
-        public bool AddRating(string productId, int rating)
-        {
-            // If the ProductID is invalid, return
-            if (string.IsNullOrEmpty(productId))
-            {
-                return false;
-            }
-
-            var products = GetAllData();
-
-            // Look up the product, if it does not exist, return
-            var data = products.FirstOrDefault(x => x.Id.Equals(productId));
-            if (data == null)
-            {
-                return false;
-            }
-
-            // Check Rating for boundaries, do not allow ratings below 0
-            if (rating < 0)
-            {
-                return false;
-            }
-
-            // Check Rating for boundaries, do not allow ratings above 5
-            if (rating > 5)
-            {
-                return false;
-            }
-
-            // Check to see if the rating exist, if there are none, then create the array
-            if(data.Ratings == null)
-            {
-                data.Ratings = new int[] { };
-            }
-
-            // Add the Rating to the Array
-            var ratings = data.Ratings.ToList();
-            ratings.Add(rating);
-            data.Ratings = ratings.ToArray();
-
-            // Save the data back to the data store
-            SaveData(products);
-
-            return true;
-        }
 
         /// <summary>
         /// Find the data record
@@ -107,14 +51,7 @@ namespace ContosoCrafts.WebSite.Services
 
             // Update the data to the new passed in values
             productData.Title = data.Title;
-            productData.Description = data.Description.Trim();
-            productData.Url = data.Url;
             productData.Image = data.Image;
-
-            productData.Quantity = data.Quantity;
-            productData.Price = data.Price;
-
-            productData.CommentList = data.CommentList;
 
             SaveData(products);
 
@@ -126,7 +63,6 @@ namespace ContosoCrafts.WebSite.Services
         /// </summary>
         private void SaveData(IEnumerable<ProductModel> products)
         {
-
             using (var outputStream = File.Create(JsonFileName))
             {
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
@@ -151,8 +87,6 @@ namespace ContosoCrafts.WebSite.Services
             {
                 Id = System.Guid.NewGuid().ToString(),
                 Title = "Enter Title",
-                Description = "Enter Description",
-                Url = "Enter URL",
                 Image = "",
             };
 
@@ -176,11 +110,10 @@ namespace ContosoCrafts.WebSite.Services
             var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
 
             var newDataSet = GetAllData().Where(m => m.Id.Equals(id) == false);
-            
+
             SaveData(newDataSet);
 
             return data;
         }
-        
     }
 }
