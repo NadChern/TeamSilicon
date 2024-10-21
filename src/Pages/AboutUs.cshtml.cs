a5c8ac13c308db6dab9c60dd300c285a17b039ac
Nadin Chernova <nchernova2702@gmail.com>
2024-10-20 20:33:25 -0700

    Renamed from Privacy.cshtml.cs to AboutUs.cshtml.cs in Pages folder

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    public class PrivacyModel : PageModel
+    public class AboutUsModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        private readonly ILogger<AboutUsModel> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public AboutUsModel(ILogger<AboutUsModel> logger)
         {
             _logger = logger;
         }
