76fb09cccf13178411568f0098c993aa57279880
Nadin Chernova <nchernova2702@gmail.com>
2024-10-24 15:23:33 -0700

    Added comments for ProductModel class in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -5,18 +5,31 @@ using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// CategoryModel represents a single category of the flashcards 
+    /// with its properties
+    /// </summary>
     public class ProductModel
     {
+        // Primary key Id for the category
         public string Id { get; set; }
-      
+        
+        // Path to category's image
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
+        // Title of the category
         [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = 
             "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
+        // Color assigned to the category of the flashcards
         public string CategoryColor { get; set; }
+        
+        /// <summary>
+        /// Converts current CategoryModel object into JSON string representation
+        /// </summary>
+        /// <returns>Json string representation of CategoryModel object</returns>
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
     }
