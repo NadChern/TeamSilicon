776a036986c398694d8d454535deede7a5dfd321
Dat Nguyen <dnguyen8@seattleu.edu>
2024-10-27 01:12:04 -0700

    Created Update.cshtml.cs file in FlashcardAdmin folder

---
+++ src/Pages/FlashcardAdmin/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.FlashcardAdmin
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
