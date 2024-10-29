904bca26d4be915ec8d01ffd40fd46c198ff6181
Dat Nguyen <dnguyen8@seattleu.edu>
2024-10-28 19:45:55 -0700

    Created ReadTests.cs for unit testing the Read.cshtml.cs file

---
+++ UnitTests/Services/ReadTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using NUnit.Framework.Legacy;
+using ContosoCrafts.WebSite.Pages.FlashcardAdmin;
+
+namespace UnitTests.Services
+{
+    /// <summary>
+    /// Unit Tests for JsonFlascardService class.
+    /// </summary>
+    public class ReadTests
+    {
+        private JsonFileFlashcardService flashcardService;
+        private ReadModel readModel;
+
+        #region OnGet
+        [Test]
+        public void OnGet_Invalid_Id_Should_Return_False()
+        {
+            // Arrange
+            flashcardService = TestHelper.FlashcardService;
+            readModel = new ReadModel(flashcardService);
+            var invalidId = "9999";
+
+            // Act
+            readModel.OnGet(invalidId);
+            var result = readModel.IsFlashcardLoaded;
+
+            // Assert
+            ClassicAssert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void OnGet_Valid_Id_Should_Return_True()
+        {
+            // Arrange
+            flashcardService = TestHelper.FlashcardService;
+            readModel = new ReadModel(flashcardService);
+            var validId = "1";
+
+            // Act
+            readModel.OnGet(validId);
+            var result = readModel.IsFlashcardLoaded;
+
+            // Assert
+            ClassicAssert.AreEqual(true, result);
+        }
+        #endregion OnGet
+
+    }
+
+}
