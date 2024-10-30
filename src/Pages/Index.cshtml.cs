0bddc00dd47f5cd725a9fab10446b55c74a252f9
Nadin Chernova <nchernova2702@gmail.com>
2024-10-29 22:44:18 -0700

    Added comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections.Generic;
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    
+    /// <summary>
+    /// Represents page, which displays a list of categories
+    /// </summary>
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// Logger instance for logging events and diagnostics related to page.
+        /// </summary>
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of IndexModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance used to log events and diagnostics</param>
+        /// <param name="categoryService">The service used to retrieve category data</param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileCategoryService categoryService)
         {
@@ -20,11 +28,24 @@ namespace ContosoCrafts.WebSite.Pages
             CategoryService = categoryService;
         }
 
+        /// <summary>
+        /// Gets the service responsible for accessing category data.
+        /// </summary>
         public JsonFileCategoryService CategoryService { get; }
+
+        /// <summary>
+        /// Gets the collection of categories to be displayed on page.
+        /// </summary>
         public IEnumerable<CategoryModel> Categories { get; private set; }
 
+
+        /// <summary>
+        /// Handles HTTP GET requests to retrieve all categories and assigns
+        /// them to Categories property.
+        /// </summary>
         public void OnGet()
         {
+            // Retrieve all categories from the service
             Categories = CategoryService.GetAllData();
         }
     }
