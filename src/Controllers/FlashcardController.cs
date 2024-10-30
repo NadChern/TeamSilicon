66d0b5b34bf3132d2071d14d8fb29d9c929935e5
Nadin Chernova <nchernova2702@gmail.com>
2024-10-29 22:45:46 -0700

    Added comments to FlashcardController.cs

--- src/Controllers/FlashcardController.cs
+++ src/Controllers/FlashcardController.cs
@@ -5,17 +5,31 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controller for managing flashcards
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class FlashcardController: ControllerBase
     {
+        /// <summary>
+        /// Initializes a new instance of FlashcardController class.
+        /// </summary>
+        /// <param name="flashcardService"></param>
         public FlashcardController(JsonFileFlashcardService flashcardService)
         {
             FlashcardService = flashcardService;
         }
         
+        /// <summary>
+        /// Gets service responsible for accessing flashcard data
+        /// </summary>
         public JsonFileFlashcardService FlashcardService { get; }
         
+        /// <summary>
+        /// Handles HTTP GET requests to retrieve all flashcards
+        /// </summary>
+        /// <returns>Collection of FlashcardModel objects representing flashcards</returns>
         [HttpGet]
         public IEnumerable<FlashcardModel> Get()
         {
