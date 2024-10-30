eaaf2454f337ac81988ded893b6764ca3330d176
Nadin Chernova <nchernova2702@gmail.com>
2024-10-29 22:42:48 -0700

    Added comments to Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -3,23 +3,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// Configures services and request pipeline for web application.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration settings for the application.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets the configuration settings for the application.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Adds services to the dependency injection container.
+        /// This method is called by the runtime.
+        /// </summary>
+        /// <param name="services">The collection of services to configure.</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            // This method gets called by the runtime. Use this method to add services to the container.
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddServerSideBlazor();
             services.AddHttpClient();
@@ -28,9 +42,15 @@ namespace ContosoCrafts.WebSite
             services.AddTransient<JsonFileFlashcardService>(); // register flashcard service
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configures the HTTP request pipeline.
+        /// This method is called by the runtime.
+        /// </summary>
+        /// <param name="app">The application builder to configure the middleware pipeline.</param>
+        /// <param name="env">The hosting environment information.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -44,11 +64,8 @@ namespace ContosoCrafts.WebSite
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
