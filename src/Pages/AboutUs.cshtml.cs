df6988bba71d7dc7f0dfa6c1d0d2b50cbdb4d7b3
Nadin Chernova <nchernova2702@gmail.com>
2024-10-29 22:46:44 -0700

    Added comments to AboutUs.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -3,15 +3,28 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Represents About Us page
+    /// </summary>
     public class AboutUsModel : PageModel
     {
+        /// <summary>
+        /// Logger instance for logging information and errors related to the About Us page.
+        /// </summary>
         private readonly ILogger<AboutUsModel> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of AboutUsModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance used to log events and diagnostics.</param>
         public AboutUsModel(ILogger<AboutUsModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Handles the HTTP GET request for the About Us page.
+        /// </summary>
         public void OnGet()
         {
         }
