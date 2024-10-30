f876fbc0914da6235684fc4f1bb2ce045d023169
Nadin Chernova <nchernova2702@gmail.com>
2024-10-29 22:43:05 -0700

    Added comments to Read.cshtml.cs

--- src/Pages/FlashcardAdmin/Read.cshtml.cs
+++ src/Pages/FlashcardAdmin/Read.cshtml.cs
@@ -4,30 +4,49 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.FlashcardAdmin
 {
+    /// <summary>
+    /// Represents Read page
+    /// </summary>
     public class ReadModel : PageModel
     {
+        /// <summary>
+        /// Gets service responsible for accessing flashcard data
+        /// </summary>
         public JsonFileFlashcardService FlashcardService { get; }
 
+        /// <summary>
+        /// Gets the flashcard data that is retrieved by OnGet method
+        /// </summary>
         public FlashcardModel Flashcard { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of ReadModel class
+        /// </summary>
+        /// <param name="flashcardService"></param>
         public ReadModel(JsonFileFlashcardService flashcardService)
         {
             FlashcardService = flashcardService;
         }
 
-        public bool IsFlashcardLoaded { get; set; } // For testing purposes
-
-        public void OnGet(string id)
         /// <summary>
-        /// OnGet gets the flashcard based on id
+        /// A flag to indicate if the flashcard has been successfully loaded.
         /// </summary>
+        public bool IsFlashcardLoaded { get; set; }
+
+        /// <summary>
+        /// Handles the HTTP GET request to retrieve a specific flashcard by its ID
+        /// </summary>
+        /// <param name="id">ID of the flashcard to be retrieved</param>
+        public void OnGet(string id)
         {
+            // Assign provided service to FlashcardService property
             Flashcard = FlashcardService.GetById(id);
             if (Flashcard == null)
             {
                 IsFlashcardLoaded = false;
                 return;
             }
+
             IsFlashcardLoaded = true;
         }
     }
