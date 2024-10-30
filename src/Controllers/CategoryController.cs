74f2f11639cc061738f33d59d2daef6f9d808289
Nadin Chernova <nchernova2702@gmail.com>
2024-10-29 22:46:20 -0700

    Added comments to CategoryController.cs

--- src/Controllers/CategoryController.cs
+++ src/Controllers/CategoryController.cs
@@ -5,17 +5,32 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controller for managing categories
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class CategoryController : ControllerBase
     {
+        /// <summary>
+        /// Initializes a new instance of the CategoryController class
+        /// </summary>
+        /// <param name="categoryService"></param>
         public CategoryController(JsonFileCategoryService categoryService)
         {
             CategoryService = categoryService;
         }
 
+        /// <summary>
+        /// Gets service that provides access to category data
+        /// </summary>
         public JsonFileCategoryService CategoryService { get; }
 
+
+        /// <summary>
+        /// Handles HTTP GET requests to retrieve all categories
+        /// </summary>
+        /// <returns> Collection of CategoryModel objects.</returns>
         [HttpGet]
         public IEnumerable<CategoryModel> Get()
         {
