edb9a954f52c5321c111792e20c9aea53e9b0bae
Nadin Chernova <nchernova2702@gmail.com>
2024-11-04 00:15:59 -0800

    Added Create.cshtml.cs in FlashcardAdmin folder

---
+++ src/Pages/FlashcardAdmin/Create.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.FlashcardAdmin
+{
+
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
