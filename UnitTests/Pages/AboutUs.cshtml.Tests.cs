630012ed08cc9cda216fd0a2b93127d77e0a6058
Dat Nguyen <dnguyen8@seattleu.edu>
2024-11-04 14:05:42 -0800

    Added unit testing for about us page in AboutUs.cshtml.Tests.cs

---
+++ UnitTests/Pages/AboutUs.cshtml.Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using NUnit.Framework.Legacy;
+using ContosoCrafts.WebSite.Pages.FlashcardAdmin;
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit tests for the AboutUsModel class.
+    /// </summary>
+    public class AboutUs
+    {
+        private AboutUsModel aboutUsModel;
+        private Mock<ILogger<AboutUsModel>> mockLogger;
+
+        #region OnGet
+        /// <summary>
+        /// Ensures OnGet executes without errors.
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_True()
+        {
+            // Arrange
+            mockLogger = new Mock<ILogger<AboutUsModel>>();
+            aboutUsModel = new AboutUsModel(mockLogger.Object);
+
+            // Act
+            aboutUsModel.OnGet();
+            var result = aboutUsModel.IsLoaded;
+
+            // Assert
+            ClassicAssert.AreEqual(true, result);
+        }
+        #endregion OnGet
+    }
+}
