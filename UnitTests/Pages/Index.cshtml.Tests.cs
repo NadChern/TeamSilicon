c63a1a0aa2c1968bbdd414ad0de863479138f3da
Nadin Chernova <nchernova2702@gmail.com>
2024-11-05 12:09:46 -0800

    Added comments to Index.cshtml.Tests.cs

--- UnitTests/Pages/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -1,26 +1,33 @@
 using System.Linq;
-
 using Microsoft.Extensions.Logging;
-
 using Moq;
-
 using NUnit.Framework;
-
 using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages
 {
+    /// <summary>
+    /// Unit tests for the IndexModel class.
+    /// </summary>
     public class IndexTests
     {
         #region TestSetup
-
+        
+        /// <summary>
+        /// The page model being tested, an instance of IndexModel.
+        /// </summary>
         public static IndexModel pageModel;
 
+        /// <summary>
+        /// Initializes the test environment for each test case.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
-        {
+        {   
+            // Create a mock logger for IndexModel
             var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
 
+            // Instantiate IndexModel with the mock logger and test category service
             pageModel = new IndexModel(MockLoggerDirect, TestHelper.CategoryService)
             {
             };
@@ -29,6 +36,10 @@ namespace UnitTests.Pages
         #endregion TestSetup
 
         #region OnGet
+        
+        /// <summary>
+        /// Tests that the OnGet method populates Categories with data and returns a valid ModelState.
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
