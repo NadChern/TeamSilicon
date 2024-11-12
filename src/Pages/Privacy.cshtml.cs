ec76847b0d1db9c1602f414974e1afa82c019d86
Dat Nguyen <dnguyen8@seattleu.edu>
2024-11-11 22:13:30 -0800

    Added back the Privacy.cshtml.cs

---
+++ src/Pages/Privacy.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        /// <summary>
+        /// Logger instance for logging information and errors related to the Privacy page.
+        /// </summary>
+        private readonly ILogger<PrivacyModel> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of PrivacyModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance used to log events and diagnostics.</param>
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// A flag indicating that OnGet was successful.
+        /// </summary>
+        public bool IsLoaded { get; set; }
+
+        /// <summary>
+        /// Handles the HTTP GET request for the Privacy page.
+        /// </summary>
+        public void OnGet()
+        {
+            IsLoaded = true;
+        }
+    }
+}
