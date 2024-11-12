a3954bdc91c8ad1b2e9fab3a2204f50e651d3ec8
Dat Nguyen <dnguyen8@seattleu.edu>
2024-11-11 16:54:12 -0800

    Added unit tests in FlashcardControllerTests.cs

---
+++ UnitTests/Controllers/FlashcardControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Controllers;
+using NUnit.Framework.Legacy;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Controllers
+{
+    public class FlashcardControllerTests
+    {
+        private JsonFileFlashcardService flashcardService;
+        /// <summary>
+        /// Get should return correct properties
+        /// </summary>
+        [Test]
+        public void Get_Should_Return_Correct_Properties()
+        {
+            // Arrange
+            flashcardService = TestHelper.FlashcardService;
+            var controller = new FlashcardController(flashcardService);
+
+            // Act
+            var result = controller.Get().First().Question;
+
+            // Assert
+            ClassicAssert.AreEqual("What are the four pillars of OOP?", result);
+        }
+    }
+}
