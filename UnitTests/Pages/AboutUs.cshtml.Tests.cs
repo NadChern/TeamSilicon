dba4d126e06ea2b4eebfc4126b70b99e415c6c41
Dat Nguyen <dnguyen8@seattleu.edu>
2024-11-12 14:08:53 -0800

    Added lines to improve readability in AboutUs.cshtml.Tests.cs

--- UnitTests/Pages/AboutUs.cshtml.Tests.cs
+++ UnitTests/Pages/AboutUs.cshtml.Tests.cs
@@ -15,6 +15,7 @@ namespace UnitTests.Pages
         private Mock<ILogger<AboutUsModel>> mockLogger;
 
         #region OnGet
+
         /// <summary>
         /// Ensures OnGet executes without errors.
         /// </summary>
@@ -32,6 +33,8 @@ namespace UnitTests.Pages
             // Assert
             ClassicAssert.AreEqual(true, result);
         }
+
         #endregion OnGet
+
     }
 }
